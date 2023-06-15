a8893a132388a7a9350ea97b13e902a7d64fe3b5
omeryilldirim <omeryilldirim@outlook.com>
2023-06-15 15:52:37 +0300

    deploy

--- src/components/InterviewAccord.jsx
+++ src/components/InterviewAccord.jsx
@@ -2,7 +2,6 @@ import Question from "./Question";
 
 
 const InterviewAccord = ({data}) => {
-    console.log(data);
   return (
     <div className="row">
         {data.map((q) => <Question data={q} key={q.id}/>)}
